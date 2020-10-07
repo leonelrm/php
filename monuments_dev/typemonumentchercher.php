b6c1c27bb7e5846bbc2b915720fcc5c5b760e7ed
leonelrm <69789534+leonelrm@users.noreply.github.com>
2020-10-07 17:19:26 +0200

    Update typemonumentchercher.php

--- monuments_dev/typemonumentchercher.php
+++ monuments_dev/typemonumentchercher.php
@@ -4,25 +4,27 @@
 	session_start();
     
     
-    $id= $_GET['id'];
-    $vId= $id;
+    $id = $_GET['id'];
+    $vId = $id;
+    echo $vId;
 
     // prepare requête sql 
-	$reqSql = "select * from type_monument where ID_TYPE_monument :vId";
+	$reqsql = "Select * from type_monument where ID_TYPE_Monument= :vId";
     
     
    	try{
 		$cnx = connect_db();
-		$stmt=$cnx->prepare($reqSql);
+		$stmt=$cnx->prepare($reqsql);
 						
 		// bind parameters
 		$stmt->bindParam(':vId', $vId, PDO::PARAM_INT);
 				
         //exécution
-        $cnx= query(reqsql);
+        //$cnx= query(reqsql);
 		$stmt->execute();
-        $typemonumenttrouve = stmt-> fetch();
-		//fermeture du curseur
+        $typemonumenttrouve = $stmt->fetch();
+        
+        //fermeture du curseur
         $stmt->closeCursor();
         
         
@@ -31,8 +33,8 @@
 	} catch(PDOException $error){
 		$message_erreur =  "Erreur SQL ! : " . $error->getCode().' '.$error->getMessage() . "<br/>";
 		$_SESSION['message_erreur'] = $message_erreur;
-		Header("Location: page_erreur.php" );
+		//Header("Location: page_erreur.php" );
 	}
     $cnx = null;
-    print_r('typemonumenttrouve');
+    print_r($typemonumenttrouve);
 ?>
